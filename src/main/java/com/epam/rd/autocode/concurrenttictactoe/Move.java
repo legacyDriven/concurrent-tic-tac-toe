0b857814a4c5753b008abb0c74e9dbaf3c6ca87c
Eugeniusz <eugenioskye@gmail.com>
2022-09-03 03:15:12 +0200

    First Commit, all good

--- src/main/java/com/epam/rd/autocode/concurrenttictactoe/Move.java
+++ src/main/java/com/epam/rd/autocode/concurrenttictactoe/Move.java
@@ -1,5 +1,8 @@
 package com.epam.rd.autocode.concurrenttictactoe;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Move {
     final int row;
     final int column;
@@ -7,5 +10,11 @@ public class Move {
     public Move(final int row, final int column) {
         this.row = row;
         this.column = column;
+    }
+
+    public static void main(String[] args) {
+        Map<Object,String> values = new HashMap<>();
+        values.put("1","2");
+        System.out.println(values.get(1));
     }
 }
